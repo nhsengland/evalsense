121c2f5c1ab9dfa260eafb41ec656d5e55d204da
Adam Dejl <adam.dejl18@imperial.ac.uk>
2025-09-10 19:43:40 +0100

    Implement web UI for model configuration, revamp state management

--- evalsense/webui/components/execution.py
+++ evalsense/webui/components/execution.py
@@ -1,8 +1,6 @@
-from dataclasses import asdict
-
 import gradio as gr
 
 
 def execution_tab(state: gr.State):
     gr.Markdown("Use this tab to execute the selected evaluation.")
-    gr.JSON(lambda state: asdict(state), inputs=[state], label="Current State")
+    gr.JSON(lambda state: state, inputs=[state], label="Current State", open=True)
